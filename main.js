35acad0d01be0aa0bc225e60e214b9575bd3ebfa
Excel Bill <60159086+Kessyblaise1@users.noreply.github.com>
2021-10-09 21:52:23 +0100

    first commit

--- main.js
+++ main.js
@@ -1,18 +1,58 @@
+let dialy = document.querySelector(".daily");
+
 fetch('data.json')
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
-        appendData(data);
-    })
+    // .then(function (data) {
+    //     // document.getElementById("daily").addEventListener("click", loadDaily(data));
+    //     // document.getElementById("weekly").addEventListener("click", appendData(data));
+    //     loadDaily(data)
+    // })
+    .then(document.getElementById("daily", function () {appendData(data)}))
     .catch(function (err) {
         console.log(err);
     });
 
-    let title = document.querySelector(".task")
-    function cowbell(value){
-        console.log(value.title)
-    }
     function appendData(data) {
-        let titles = data.map().join('')
+        
+        let management = document.querySelector(".management").innerHTML = `
+            ${data.map(function (card) {
+                return `
+                    <div class="task_container ${card.title.split(' ').join('')}">
+                        <div class="task_card rounded">
+                            <div class="task">
+                                <p class="title">${card.title}</p>
+                                <i class='bx bx-dots-horizontal-rounded'></i>
+                            </div>
+                            <div class="time">
+                                <p class="current">${card.timeframes.weekly.current}hrs</p>
+                                <p class="previous">Last Week - <span class="previous">${card.timeframes.weekly.previous}hrs</span></p>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }).join("")}
+        `
+    }
+
+    function loadDaily(data) {
+        let management = document.querySelector(".management").innerHTML = `
+            ${data.map(function (card) {
+                return `
+                    <div class="task_container ${card.title.split(' ').join('')}">
+                        <div class="task_card rounded">
+                            <div class="task">
+                                <p class="title">${card.title}</p>
+                                <i class='bx bx-dots-horizontal-rounded'></i>
+                            </div>
+                            <div class="time">
+                                <p class="current">${card.timeframes.daily.current}hrs</p>
+                                <p class="previous">Last Week - <span class="previous">${card.timeframes.daily.previous}hrs</span></p>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }).join("")}
+            `
     }
